fefaee20973deeeb1a4f03eac9f089633bec649a
tejohnso <mail@tyler-johnson.ca>
2014-08-31 22:18:59 -0400

    Update gapi and oauth functionality

---
+++ web/js/gapi-initial-onload.js
@@ -0,0 +1,43 @@
+"use strict";
+/* global handleClientJSLoad: false */
+/* jshint unused: false */
+function handleClientJSLoad() {
+  console.log("Google JS client is loaded");
+  angular.element(document).ready(function() {
+    angular.element(document).injector()
+    .invoke(["gapiClientService", "$window", "STORAGE_URL", "$q", "$log",
+    function(gapiClient, $window, STORAGE_URL, $q, $log) {
+      return loadStorageClient()
+             .then(function() {
+                return gapiClient.fulfill($window.gapi.client);
+              });
+
+      function loadStorageClient() {
+        var defer = $q.defer();
+        $window.gapi.client.load("storage", "v0.01", function () {
+          if ($window.gapi.client.storage) {
+            $log.info("Storage API is loaded");
+            defer.resolve();
+          } else {
+            $log.error("Storage API is NOT loaded");
+            defer.reject();
+          }
+        }, STORAGE_URL);
+
+        return defer.promise;
+      }
+    }]);
+  });
+}
+angular.module("gapi", [])
+.factory("gapiClientService", ["$q", function ($q) {
+  var svc = {};
+  var defer = $q.defer();
+  svc.get= function () {
+    return defer.promise;
+  };
+  svc.fulfill = function(gapiClient) {
+    return defer.resolve(gapiClient);
+  };
+  return svc;
+}]);
