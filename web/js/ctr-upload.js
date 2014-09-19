3ad6853aaf66d69fec071db0aab10ecec8ce2bed
tejohnso <mail@tyler-johnson.ca>
2014-09-19 19:23:41 -0400

    Add proper status message to upload bar

--- web/js/ctr-upload.js
+++ web/js/ctr-upload.js
@@ -5,35 +5,37 @@ function ($scope, $route, $routeParams, $http, FileUploader, uriSvc, filesSvc) {
   var uploader = $scope.uploader = new FileUploader();
 
   uploader.method = "PUT";
+  $scope.status = {};
 
   uploader.onAfterAddingFile = function(fileItem) {
     console.info("onAfterAddingFile", fileItem);
-    $scope.uploadActive = true;
     if ($routeParams.folder) {
       fileItem.file.name = $routeParams.folder + "/" + fileItem.file.name;
     }
 
-    $scope.uploadStatusMessage = "Requesting permission";
+    $scope.status.message = "Requesting permission";
 
     uriSvc.getURI($routeParams.companyId, encodeURIComponent(fileItem.file.name))
     .then(function(resp) {
       fileItem.url = resp;
       uploader.uploadItem(fileItem);
+    })
+    .then(null, function(resp) {
+      $scope.status = resp;
     });
   };
 
   uploader.onBeforeUploadItem = function(item) {
-    $scope.uploadStatusMessage = "Uploading " + item.file.name;
+    $scope.status.message = "Uploading " + item.file.name;
   };
 
   uploader.onCancelItem = function(item) {
     uploader.removeFromQueue(item);
-    if (uploader.queue.length === 0) {$scope.uploadActive = false;}
   };
 
   uploader.onCompleteItem = function(item) {
     if (item.isCancel) {return;}
-    $scope.uploadStatusMessage = "Verifying " + item.file.name;
+    $scope.status.message = "Verifying " + item.file.name;
 
     function verifySize(size) {
       return parseInt(size) === item.file.size;
@@ -44,7 +46,7 @@ function ($scope, $route, $routeParams, $http, FileUploader, uriSvc, filesSvc) {
                 $routeParams.companyId + "/o/" + encodeURIComponent(item.file.name)})
     .then(function(resp) {
       if (!resp.data || !verifySize(resp.data.size)) {
-        $scope.uploadStatusMessage = "Upload did not complete";
+        $scope.status.message = "Upload did not complete";
         item.isError = true;
         return;
       }
@@ -52,10 +54,9 @@ function ($scope, $route, $routeParams, $http, FileUploader, uriSvc, filesSvc) {
       filesSvc.addFile(resp.data);
       item.isSuccess = true;
       uploader.removeFromQueue(item);
-      if (uploader.queue.length === 0) {$scope.uploadActive = false;}
     }, function(err) {
       console.log(err);
-      $scope.uploadStatusMessage = "Could not verify";
+      $scope.status.message = "Could not verify";
     });
   };
 }]);
