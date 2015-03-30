900d9e58059e3b48f481cfbebc37046ec0522221
tejohnso <mail@tyler-johnson.ca>
2015-03-30 12:58:37 -0400

    Fix sub company switch on differen page sizes

--- web/js/subscription/subscription-e2e.js
+++ web/js/subscription/subscription-e2e.js
@@ -4,7 +4,7 @@ var until = require("selenium-webdriver").until,
 webdriver = require("selenium-webdriver"),
 
 locators = {
-  "userProfile": {"xpath": "//div[@class='user-id']/span[1]"},
+  "userProfile": {"xpath": "//div[contains(@class,'user-id')]/span"},
   "selectSubCompany": {"xpath": "//a[text()='Select Sub-Company']"},
   "setCompany": {"css": "div[ng-click='setCompany(company)']"},
   "homeCompany": {"css": "a[ng-click='switchToMyCompany()']"},
@@ -12,23 +12,26 @@ locators = {
   "subs": {"xpath": "//div[@id='subscription-status']//span[contains(.,'Subs')]"},
 };
 
-function findVisibleSubCompanyLink(driver) {
-  var links = driver.findElements(locators.selectSubCompany);
-  return webdriver.promise.filter(links, function(link) {
-    return link.isDisplayed();
-  }).then(function(visibleLink) {
-    return visibleLink[0];
-  });
+function findVisibleSelector(selector) {
+  return function findWithDriver(driver) {
+    console.log("Finding visible among " + JSON.stringify(selector));
+    var links = driver.findElements(selector);
+    return webdriver.promise.filter(links, function(link) {
+      return link.isDisplayed();
+    }).then(function(visibleLinks) {
+      return visibleLinks[0];
+    });
+  }
 }
 
 module.exports = function(driver) {
   driver.wait(until.elementLocated(locators.subs), 5000, "initial subscribed");
 
   driver.wait(until.elementLocated(locators.userProfile), 5000, "profile");
-  driver.findElement(locators.userProfile).click();
+  driver.findElement(findVisibleSelector(locators.userProfile)).click();
 
   driver.wait(until.elementLocated(locators.selectSubCompany), 5000, "drop down");
-  driver.findElement(findVisibleSubCompanyLink).click();
+  driver.findElement(findVisibleSelector(locators.selectSubCompany)).click();
 
   driver.wait(until.elementLocated(locators.setCompany), 9000, "company listing");
   driver.findElement(locators.setCompany).click();
