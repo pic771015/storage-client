7c9d2c07d205016bf1c0b5e926d658772db2bd51
tejohnso <mail@tyler-johnson.ca>
2015-04-17 19:18:44 -0400

    Remove modal.html - incorporate into index.html

--- web/js/modal/ctr-modal.js
+++ web/js/modal/ctr-modal.js
@@ -1,9 +1,11 @@
 "use strict";
 
 angular.module("risevision.storage.modal", [])
-.controller("ModalWindowController", ["$scope", function($scope) {
+.controller("ModalWindowController", ["$scope", "FULLSCREEN",
+function($scope, FULLSCREEN) {
+  $scope.FULLSCREEN = FULLSCREEN;
+
   $scope.closeButtonClick = function() {
     gadgets.rpc.call("", "rscmd_closeSettings", null);
   };
-}])
-;
+}]);
