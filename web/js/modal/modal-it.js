49f37279cfe1f92e25bd56c95af4d9639da647c3
tejohnso <mail@tyler-johnson.ca>
2015-04-19 14:29:25 -0400

    Improve function naming

--- web/js/modal/modal-it.js
+++ web/js/modal/modal-it.js
@@ -1,23 +1,28 @@
 "use strict";
 var until = require("selenium-webdriver").until;
 
-function close() {
+function closeStorageModal() {
   document.querySelector("iframe")
   .contentDocument.querySelector("button[ng-click='closeButtonClick()']")
   .click();
 }
 
-function verifyClose() {
+function isModalOpened() {
+  return document.querySelector("iframe")
+  .contentDocument.querySelector("button[ng-click='closeButtonClick()']")
+}
+
+function isModalClosed() {
   return document.querySelectorAll("iframe").length === 0;
 }
 
 module.exports = function(driver) {
   driver.get("http://localhost:8000/test/integration/iframe-test.html");
-  driver.wait(until.elementLocated({"css": "iframe"}), 2000, "iframe");
+  driver.waitForTruthyScript(isModalOpened, 5000, "modal opened");
 
-  driver.executeScript(close);
+  driver.executeScript(closeStorageModal);
 
-  driver.waitForTruthyScript(verifyClose, 5000, "modal not closed");
+  driver.waitForTruthyScript(isModalClosed, 5000, "modal not closed");
 
   driver.logMessage("modal was closed as expected");
 };
