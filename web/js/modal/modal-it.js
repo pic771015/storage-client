667709d76495d47fdead4664a5c4d07ceca7f371
tejohnso <mail@tyler-johnson.ca>
2015-04-19 15:19:23 -0400

    Fix linting

--- web/js/modal/modal-it.js
+++ web/js/modal/modal-it.js
@@ -1,5 +1,4 @@
 "use strict";
-var until = require("selenium-webdriver").until;
 
 function closeStorageModal() {
   document.querySelector("iframe")
@@ -9,7 +8,7 @@ function closeStorageModal() {
 
 function isModalOpened() {
   return document.querySelector("iframe")
-  .contentDocument.querySelector("button[ng-click='closeButtonClick()']")
+  .contentDocument.querySelector("button[ng-click='closeButtonClick()']");
 }
 
 function isModalClosed() {
